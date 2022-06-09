49da71f874fbf662296e6064f016ca3eaec05f34
fmstacco <fmstacco@gmail.com>
2022-06-09 18:22:07 +0000

    Add function calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,9 +41,22 @@ function runGame(gameType) {
 function checkAnswer() {
 
 }
-
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+//We are using parseInt function to make sure we treat the value as a number
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+   }else{
+       alert(`Unimplemented operator ${operator}`);
+       throw `Unimplemented operator ${operator}.Aborting!`;
+   }
 }
 
 function incrementScore() {
@@ -59,6 +72,8 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
 }
+
+// It's good practice to avoid global variables
 
 function displaySubtractQuestion() {
     
